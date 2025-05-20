356de5542217a662b53a8d541160808acce722c5
NathanDynamix <164498129+SowndharyaBala@users.noreply.github.com>
2025-05-20 23:22:59 +0530

    INTRO

---
+++ src/datatype/Data_Type.java
@@ -0,0 +1,24 @@
+package datatype;
+
+public class Data_Type {
+    public static void main(String[] args){
+        short marks = 156;  // -128 to 127
+        System.out.println(marks);
+
+        byte age = 67;
+        System.out.println(age);
+
+        boolean lunch = false;
+        System.out.println(lunch);
+
+        char letter = 'y';
+        System.out.println(letter);
+
+        long num = 89651L;
+        System.out.println(num);
+
+        double perc = 67.89d;
+        System.out.println(perc);
+
+    }
+}
---
+++ src/introduction/Introduction.java
@@ -0,0 +1,21 @@
+package introduction;
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java class");
+        System.out.println(21);
+        System.out.print("true");
+        System.out.println(54);
+        System.out.print("Besant");
+        System.out.println("Sowndharya");
+
+//       Welcome
+        //21true
+        //54Besant
+        //Sowndharya
+
+        //welcome
+        //21
+        //true54
+        //Besantsowndharya
+    }
+}
