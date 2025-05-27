149e0a730363be148906eb47b6ee816676997ae2
NathanDynamix <164498129+SowndharyaBala@users.noreply.github.com>
2025-05-27 14:33:09 +0530

    casting

---
+++ src/com/casting/Explicit_Casting.java
@@ -0,0 +1,14 @@
+package com.casting;
+
+public class Explicit_Casting {
+    public static void main(String[] args) {
+        int a = 678;
+        byte b = (byte) a;
+        System.out.println(b);
+
+        double d = 728.089d;
+        byte i = (byte) d;
+        System.out.println(i);
+
+    }
+}
