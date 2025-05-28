bf6ab22120489f611d10214ccaa40d8cacea5285
NathanDynamix <164498129+SowndharyaBala@users.noreply.github.com>
2025-05-28 20:04:30 +0530

    switch

---
+++ src/com/control/Switch.java
@@ -0,0 +1,35 @@
+package com.control;
+
+public class Switch {
+    public static void main(String[] args){
+
+        int days = 8;
+
+        switch (days){
+            case 1 :
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4 :
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+
+            default:
+                System.out.println("please enter a valid day");
+        }
+    }
+}
