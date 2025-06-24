0ed035c3f460995ab5da475312eb814499ed3ec7
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-06-24 20:01:41 +0530

    bitwise

---
+++ src/com/operations/Bitwise_Operators.java
@@ -0,0 +1,17 @@
+package com.operations;
+
+public class Bitwise_Operators {
+    public static void main(String[] args){
+        //and operation
+
+        int a = 14;
+        int b = 6;
+        System.out.println(a & b);
+
+        //or operator
+        System.out.println(a | b);
+
+        //exclusively OR  (XOR)
+        System.out.println(a^b);
+    }
+}
