405b2eb1bc99cee920e0ee5310b7c46b109b1076
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-06-24 11:02:22 +0530

    methods

---
+++ src/com/books/Main.java
@@ -0,0 +1,21 @@
+package com.books;
+
+import com.java_book.j_book;
+
+public class Main {
+    public static void main(String[] args){
+//        Books b = new Books();
+//        b.pages = 300;
+//        Books.bookName = "Coding";
+//        b.reading();
+
+
+//        Java_Book j = new Java_Book();
+//        Books.bookName = "JAVA";
+//        j.pages = 700;
+//        j.show();
+
+        j_book book = new  j_book();
+        book.display();
+    }
+}
