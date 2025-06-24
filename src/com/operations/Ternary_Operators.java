a4a6e16f63ac6a41cdf05f69a2d90993e88d5614
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-06-24 20:01:54 +0530

    ternary

---
+++ src/com/operations/Ternary_Operators.java
@@ -0,0 +1,18 @@
+package com.operations;
+
+public class Ternary_Operators {
+    public static void main(String[] args){
+        int marks = 7;
+//        if(marks >=35){
+//            System.out.println("pass mark");
+//        }else
+//            System.out.println("fail mark");
+
+        String result = (marks >=35) ? "Pass" : "fail";
+        System.out.println(result);
+    }
+}
+
+
+//find largest number from a and b
+//find largest number from a, b and c
