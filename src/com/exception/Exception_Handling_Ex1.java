723a5ecd644b132c5afca990aa5da0bde8da0f91
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-07-08 20:01:46 +0530

    exception

---
+++ src/com/exception/Exception_Handling_Ex1.java
@@ -0,0 +1,25 @@
+package com.exception;
+
+public class Exception_Handling_Ex1 {
+    public static void main(String [] args){
+
+        int a = 15;
+        int b = 0;
+//       int result = a/b;
+//
+//        System.out.println(result);
+//
+//        System.out.println("The code finished execution");
+
+        try {
+            int c = a/b;
+            System.out.println(c);
+        }catch (ArithmeticException ex){
+            System.out.println(ex);
+        }finally {
+            System.out.println("I am from finally block");
+        }
+
+      //  System.out.println("I am finished the execution");
+    }
+}
