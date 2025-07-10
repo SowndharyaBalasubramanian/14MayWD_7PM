ebd31e6f80c9d8909d11863d7045f4ba142e9083
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-07-10 08:03:20 +0530

    string buffer

--- src/com/Stringg/StringBuffer_Ex.java
+++ src/com/Stringg/StringBuffer_Ex.java
@@ -7,7 +7,21 @@ public class StringBuffer_Ex {
         sbr.append(" is a training institute");
         System.out.println(sbr);
 
+        sbr.insert(5 , "!");
+        System.out.println(sbr);
+
+        sbr.replace(9, 15, "java");
+        System.out.println(sbr);
+
+        sbr.delete(9 , 15);
+        System.out.println(sbr);
+
+        sbr.reverse();
+        System.out.println(sbr);
+
         //not safe for multithreading scenarios
     }
 
 }
+
+//string < stringbuffer < stringbuilder
