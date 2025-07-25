ee56a3fdbcbc2e8a84dc42d41b521fe884781e58
SowndharyaBalasubramanian <sowndharya0105@gmail.com>
2025-07-25 08:21:50 +0530

    Wrapper class

---
+++ src/Wrapper_Class.java
@@ -0,0 +1,23 @@
+public class Wrapper_Class {
+    public static void main(String[] args){
+                double d =78.23d;
+        System.out.println(d);
+                Double Obje = d;
+        System.out.println(Obje);
+        //autoboxing   => primitive to object
+        //unboxing     => object to primitive
+
+
+        Integer Sample = 345;
+        int var = Sample;
+        System.out.println(var);
+    }
+}
+
+//int  => Intege
+//char => Character
+//byte => Byte
+//long => Long
+//short => Short
+//float => Float
+//double => Double
